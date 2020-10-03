a661a65e9049c8d28b3ba304468ce0a65bcaf8d2
Oleg Mostovenko <dev.OlegM@gmail.com>
2020-10-03 12:00:10 +0300

    first commut

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+  decr = 10;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+let test = 5 % 2;
+
+console.log(test === 1);
+
+console.log("some new changes");
